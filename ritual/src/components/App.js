1a1a7aced8deb3f280ac525e2f55793d84a3d215
Josh <joshpitzalis@gmail.com>
2016-12-22 15:40:12 +0530

    all tasks now uncheck at midnight

--- ritual/src/components/App.js
+++ ritual/src/components/App.js
@@ -3,6 +3,7 @@ import Streak from './Streak';
 import Task from './Task';
 import Add from './Add';
 import 'console-dot-frog';
+import later from 'later';
 
 class App extends Component {
 
@@ -10,13 +11,11 @@ class App extends Component {
     super();
     this.createTask = this.createTask.bind(this);
     this.updateTask = this.updateTask.bind(this);
-    this.resetTasks = this.resetTasks.bind(this);
     this.state = {
       tasks: {},
       streak: 0
     };
   }
-
 
   createTask (task) {
     const tasks = {...this.state.tasks};
@@ -28,36 +27,37 @@ class App extends Component {
   updateTask (key, updatedTask) {
     const tasks = {...this.state.tasks};
     tasks[key] = updatedTask;
-    this.setState({ tasks }, function updateStreak() {
+    this.setState({ tasks }, function updateStreak () {
       // if all tasks are complete increment streak by one, just checcking status of all tasks here
       // SuperHack! did ths by putting each completed value in an array and then squishing into a string and then checking if it includes teh word true
       const tally = Object
         .keys(this.state.tasks)
         .map(task => this.state.tasks[task].complete).toString();
-      if (!tally.includes('false')){
+      if (!tally.includes('false')) {
         let streak = {...this.state.streak};
-        streak = this.state.streak + 1
+        streak = this.state.streak + 1;
         this.setState({ streak });
       }
     });
   }
 
-  resetTasks() {
-    // all them tasks reset at midnight
-    // console.frog();
+  componentDidMount() {
+    // 1.get all current tasks and then update complete to false on all of them
+    var self = this;
+    function refreshTasks(){
+      self.setState(
+        Object.keys(self.state.tasks).map(task => {
+          self.state.tasks[task].complete = false;
+          self.state.tasks[task].disabled = false;
+        })
+      );
+    }
 
-    // 1.get all current tasks
-    this.setState(
-      Object.keys(this.state.tasks).map( task => {
-        this.state.tasks[task].complete = false;
-        this.state.tasks[task].disabled = false;
-        }
-      )
-    )
+    // 3. schedule the above with later.js,
+    var s = later.parse.text('at 00:00 am');
+    // 4.and set to local time
+    later.setInterval(refreshTasks, s);
 
-    // 2.update complete to false on all of them
-//
-    // 3. schedule teh above with later,and set to local time
   }
 
   render () {
@@ -79,7 +79,6 @@ class App extends Component {
           }
         </ol>
         <Add createTask={this.createTask} />
-        <button onClick={() => this.resetTasks()}>Clear All Tasks</button>
       </div>
     );
   }
