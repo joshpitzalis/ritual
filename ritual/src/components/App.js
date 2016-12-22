033550c181a98c58df6c2d478aa57b91bbad699d
Josh <joshpitzalis@gmail.com>
2016-12-22 17:31:01 +0530

    Streak resets to zero or increments each day

--- ritual/src/components/App.js
+++ ritual/src/components/App.js
@@ -11,37 +11,16 @@ class App extends Component {
     super();
     this.createTask = this.createTask.bind(this);
     this.updateTask = this.updateTask.bind(this);
+    this.updateStreak = this.updateStreak.bind(this);
     this.state = {
       tasks: {},
-      streak: 0
+      streak: 0,
+      updatedToday: false
     };
   }
 
-  createTask (task) {
-    const tasks = {...this.state.tasks};
-    const timestamp = Date.now();
-    tasks[`task-${timestamp}`] = task;
-    this.setState({ tasks });
-  }
-
-  updateTask (key, updatedTask) {
-    const tasks = {...this.state.tasks};
-    tasks[key] = updatedTask;
-    this.setState({ tasks }, function updateStreak () {
-      // if all tasks are complete increment streak by one, just checcking status of all tasks here
-      // SuperHack! did ths by putting each completed value in an array and then squishing into a string and then checking if it includes teh word true
-      const tally = Object
-        .keys(this.state.tasks)
-        .map(task => this.state.tasks[task].complete).toString();
-      if (!tally.includes('false')) {
-        let streak = {...this.state.streak};
-        streak = this.state.streak + 1;
-        this.setState({ streak });
-      }
-    });
-  }
-
-  componentDidMount() {
+  componentDidMount () {
+    // # uncheck all task each day
     // 1.get all current tasks and then update complete to false on all of them
     var self = this;
     function refreshTasks(){
@@ -55,9 +34,60 @@ class App extends Component {
 
     // 3. schedule the above with later.js,
     var s = later.parse.text('at 00:00 am');
+
+    // var s = later.parse.text('every 5 sec');
+    // use this for testing
+
     // 4.and set to local time
     later.setInterval(refreshTasks, s);
 
+    // # Reset the streak at midnight
+
+
+
+    function refreshStreak () {
+      // if updatedToday = false then reset streak
+      if (self.state.updatedToday === false) {
+        let streak = {...self.state.streak};
+        const reset = 0;
+        streak = reset;
+        self.setState(
+          {streak}
+        );
+      }
+    }
+
+    later.setInterval(refreshStreak, s);
+  }
+
+  createTask (task) {
+    const tasks = {...this.state.tasks};
+    const timestamp = Date.now();
+    tasks[`task-${timestamp}`] = task;
+    this.setState({ tasks });
+  }
+
+  updateStreak () {
+    // if all tasks are complete increment streak by one, just checcking status of all tasks here
+    // SuperHack! did ths by putting each completed value in an array and then squishing into a string and then checking if it includes teh word true
+    const tally = Object
+      .keys(this.state.tasks)
+      .map(task => this.state.tasks[task].complete).toString();
+    if (!tally.includes('false')) {
+      let streak = {...this.state.streak};
+      streak = this.state.streak + 1;
+      this.setState({ streak });
+    }
+    // change state to show streak has been updated so that it knows wheher to reset at midnight or not
+    this.setState({
+      updatedToday: true
+    });
+  }
+
+  updateTask (key, updatedTask) {
+    const tasks = {...this.state.tasks};
+    tasks[key] = updatedTask;
+    this.setState({ tasks }, this.updateStreak);
   }
 
   render () {
